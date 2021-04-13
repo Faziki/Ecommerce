ae420771e7af6cde33e4dd27598ff696e2d56c17
Faziki <pieteruys33@gmail.com>
2021-04-14 00:09:12 +0200

    added redux and created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
-
 import Header from "./Components/nav/Header";
+import RegisterComplete from "./pages/auth/RegisterComplete";
+
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -27,25 +27,8 @@ const Register = () => {
     setEmail("");
   };
 
-  //need to find out how to pass the on submit to the notifcation
-  //else this will trigger everytime you press 'register' button withou validating
-  // that the data has been sent
-
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
-      {/* <div className="form-group">
-        <label id="nameInput" className="bmd-label-floating">
-          Name
-        </label>
-        <input
-          autoFocus
-          type="text"
-          className="form-control"
-          id="nameInput"
-        ></input>
-        <span className="bmd-help">Please type in your name</span>
-      </div> */}
-
       <div className="form-group">
         <label id="emailAddress" className="bmd-label-floating">
           Email address
@@ -60,31 +43,6 @@ const Register = () => {
         ></input>
         <span className="bmd-help">Please type in your email address</span>
       </div>
-      {/* <div className="form-group">
-        <label id="passwordInput" className="bmd-label-floating">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="passwordInput"
-        ></input>
-        <span className="bmd-help">
-          Please type in a strong password (Make use of captial letters and
-          special characters $%@#)
-        </span>
-      </div>
-      <div className="form-group">
-        <label id="passwordRepeat" className="bmd-label-floating">
-          Repeat Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="passwordRepeat"
-        ></input>
-        <span className="bmd-help">Please retype your password as above</span>
-      </div> */}
       <button type="submit" className="btn btn-raised">
         Register
       </button>
